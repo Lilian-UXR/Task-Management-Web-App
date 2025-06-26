4eaa7b7c452c3e8058bd53ea2346f15e7663e872
Lilian-UXR <llim712@gmail.com>
2025-06-26 10:53:24 -0700

    Updating contact form
    added in contact form to app

--- my-todo-app/src/App.js
+++ my-todo-app/src/App.js
@@ -1,7 +1,9 @@
 import ToDo from "./Components/todo.jsx";
+import ContactForm from "./Components/contact.jsx";
 
 function App() {
   return <ToDo />;
+  <ContactForm />;
 }
 
 export default App;
---
+++ my-todo-app/src/Components/contact.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+function ContactForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(inputs);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul className="contact-form">
+        <li>
+          <label>
+            First name:
+            <input
+              type="text"
+              name="firstname"
+              value={inputs.firstname || ""}
+              onChange={handleChange}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            Last name:
+            <input
+              type="text"
+              name="lastname"
+              value={inputs.lastname || ""}
+              onChange={handleChange}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={inputs.email || ""}
+              onChange={handleChange}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            Message:
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </li>
+        <input type="submit" />
+      </ul>
+    </form>
+  );
+}
+
+export default ContactForm;
