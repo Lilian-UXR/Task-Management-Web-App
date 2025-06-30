4d933a35668e346ede943ca2385649238f7262ee
Lilian-UXR <llim712@gmail.com>
2025-06-30 09:10:36 -0700

    updating menu and pages
    Update to link pages and create menu

--- my-todo-app/src/App.js
+++ my-todo-app/src/App.js
@@ -1,9 +1,27 @@
+import Home from "./Components/Home.jsx";
 import ToDo from "./Components/todo.jsx";
 import ContactForm from "./Components/contact.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <ToDo />;
-  <ContactForm />;
+  return (
+    <div className="App">
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/todo">To Do</Link>
+            <Link to="/contact">Contact Form</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todo" element={<ToDo />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
