b32b8750f12a847411cc9c04fac2fbea5b83e113
Lilian-UXR <llim712@gmail.com>
2025-06-30 12:06:21 -0700

    Update mobile menu
    mobile menu code update for mobile menu style

--- my-todo-app/src/Components/hamburger.jsx
+++ my-todo-app/src/Components/hamburger.jsx
@@ -1,10 +1,44 @@
 import React, { useState } from "react";
 import HamburgerIcon from "../images/Hamburger.png";
+import { Link } from "react-router-dom";
+import House from "../images/House.png";
+import Contact from "../images/Contact.png";
+import ToDoList from "../images/To Do List.png";
 
 function Hamburger() {
+  const [isOpen, setisOpen] = useState(false);
+
   return (
     <nav className="mobile">
-      <img src={HamburgerIcon} alt="hamburger menu" />
+      <div className="mobile-wrapper">
+        <img src={HamburgerIcon} alt="hamburger menu" />
+        <ul className="dropdown">
+          <li>
+            <Link to="/">
+              <div className="mobile-link">
+                <img src={House} alt="house icon" />
+                <p>Home</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/todo">
+              <div className="mobile-link">
+                <img src={ToDoList} alt="to do icon" />
+                <p>To Do</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              <div className="mobile-link">
+                <img src={Contact} alt="contact icon" />
+                <p>Contact Form</p>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
